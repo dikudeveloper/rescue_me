064ebab0cb45ef7145e639ed541d6c4a994e29dd
Joseph Diku <joseph.diku@gmail.com>
2019-09-13 17:46:18 +0300

    modify endpoints

--- rescue_me/urls.py
+++ rescue_me/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rescue_me.api import views
 
 urlpatterns = [
-    path('api/rescue_me/', views.RescueMeProfileList),
-    path('api/rescue_me/<int:pk>/', views.RescueMeProfileDetail),
+    path('api/rescue_me/', views.RescueMeProfileList.as_view()),
+    path('api/rescue_me/<int:pk>/', views.RescueMeProfileDetail.as_view()),
 ]
